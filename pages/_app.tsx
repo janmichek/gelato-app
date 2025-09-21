39d437d35647c97df7a6364c9fd619a5fb477317
Jan Michek <jan@janmichek.cz>
2025-09-21 11:42:37 +0200

    add project files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
-import "@/styles/globals.css";
+import { GelatoSmartWalletProvider } from "@gelatonetwork/smartwallet-react-wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import { WagmiProvider } from "wagmi";
+
+import { config } from "../lib/wagmi";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  const gelatoApiKey = process.env.NEXT_PUBLIC_GELATO_API_KEY;
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <GelatoSmartWalletProvider
+          params={{
+            apiKey: gelatoApiKey,
+            scw: {
+              eip7702: false,
+              type: "kernel"
+            }
+          }}
+        >
+          <Component {...pageProps} />
+        </GelatoSmartWalletProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
